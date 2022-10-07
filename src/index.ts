45af080ddf979bdc4003078d010d48c5989334fc
Lindsey Jacks <linzjax@gmail.com>
2022-10-07 13:23:33 -0400

    add a lookup route

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,14 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import lookup from './handlers/lookup';
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-}
+import { Router } from 'itty-router';
 
-export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hey what's up");
-	},
-};
+// Create a new router
+const router = Router();
+
+router.post("/lookup", lookup);
+
+router.all("*", () => new Response("404, not found!", { status: 404 }))
+
+addEventListener('fetch', (e) => {
+  e.respondWith(router.handle(e.request))
+})
