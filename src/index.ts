0dad75de4c52c045e4d941241ca73f07d6825633
Lindsey Jacks <linzjax@gmail.com>
2022-12-16 11:28:51 -0500

    Add slack oauth sdk

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Router, listen } from "worktop"
 import faunadb from "faunadb"
+import { InstallProvider } from "@slack/oauth"
 
 import plusses from "./handlers/plusses"
 import authorize from "./handlers/authorize"
@@ -11,6 +12,13 @@ const faunaClient = new faunadb.Client({
   // @ts-ignore
   secret: FAUNADB_SECRET as string,
   domain: "db.fauna.com"
+})
+
+// initialize the installProvider
+const installer = new InstallProvider({
+  clientId: process.env.SLACK_CLIENT_ID as string,
+  clientSecret: process.env.SLACK_CLIENT_SECRET as string,
+  stateSecret: "my-state-secret"
 })
 
 router.add("POST", "/plusses", plusses(faunaClient))
