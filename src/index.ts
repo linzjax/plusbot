c56bd60ced9f5cfb06d99d56fad3812b6df0708f
Lindsey Jacks <linzjax@gmail.com>
2023-08-08 11:43:28 -0400

    logging

--- src/index.ts
+++ src/index.ts
@@ -27,14 +27,6 @@ export const getContent = async (request: IRequest) => {
 }
 
 router
-  .all("*", async (request: IRequest, env, tools) => {
-    const signingSecret = env.SLACK_SIGNING_SECRET
-    const isVerifiedRequest =
-      await tools.SlackAPI.helpers.verifyRequestSignature(
-        request,
-        signingSecret
-      )
-  })
   .post("/plusses", async (request, env, tools) => {
     console.log("REQUEST", JSON.stringify(request))
     const content = await getContent(request)
@@ -57,6 +49,14 @@ export default <ExportedHandler<EnvBindings>>{
     try {
       const botAccessToken = env.SLACK_BOT_ACCESS_TOKEN
       const SlackAPI = new SlackREST({ botAccessToken })
+
+      const signingSecret = env.SLACK_SIGNING_SECRET
+      const isVerifiedRequest = await SlackAPI.helpers.verifyRequestSignature(
+        request,
+        signingSecret
+      )
+
+      console.log("isVerifiedRequest?", isVerifiedRequest)
 
       const faunaClient = new faunadb.Client({
         secret: env.FAUNADB_SECRET as string,
