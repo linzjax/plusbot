80fa2be379614d46cc35aba775e82cfb7191b59a
Lindsey Jacks <linzjax@gmail.com>
2023-08-09 11:42:50 -0400

    Slight refactor of user parsing. Move stable variables functions outside of handler async function

--- src/handlers/plusses.ts
+++ src/handlers/plusses.ts
@@ -2,21 +2,69 @@ import { getFaunaError, getId, getDataField } from "../faunaUtils"
 import invariant from "tiny-invariant"
 import faunadb from "faunadb"
 
+const coreValues = [
+  ":be_kind:",
+  ":bring_the_magic:",
+  ":think_it_through:",
+  ":keep_it_simple:",
+  ":find_a_way:",
+  ":own_it:",
+  ":be_authentic:"
+]
+
+const celebrateEmojis = [
+  ":highfive:",
+  ":dancing-mage:",
+  ":bananadance:",
+  ":party_blob:",
+  ":party_cat:",
+  ":partycorgi:",
+  ":dancing-dog:",
+  ":mario_luigi:",
+  ":party-doge:",
+  ":party_parrot:",
+  ":excited:",
+  ":more_cowbell:"
+]
+
+const successMessage = [
+  "Plusses for you",
+  "Way to go",
+  "Rock on",
+  "Keep it up",
+  "High five",
+  "Look at you go",
+  "You're doing great"
+]
+
+/***
+ * Parse body of the message to see the users that were tagged for plusses
+ **/
+const parseUsers = (text: string) => {
+  return text
+    .split(" ")
+    .filter((user: string) => user.includes("<@"))
+    .map((user: string) => {
+      const userArray = user.split("|")
+      const userId = userArray[0].slice(2, userArray[0].length)
+      const username = userArray[1].split(">")[0]
+
+      return {
+        id: userId,
+        username: username
+      }
+    })
+}
+
+const getRandomValue = (array: string[]) => {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
 export default async (body: any, faunaClient: faunadb.Client) => {
   try {
     const q = faunadb.query
 
-    // Parse body of the message to see the users that were tagged for plusses
-    const plussesFor = body.text
-      .split(" ")
-      .filter((user: string) => user.includes("<@"))
-      .map((user: string) => {
-        const userArray = user.split("|")
-        return {
-          id: userArray[0].slice(2, userArray[0].length),
-          username: userArray[1].slice(0, userArray[1].length - 1)
-        }
-      })
+    const plussesFor = parseUsers(body.text)
 
     const messages = await Promise.all(
       plussesFor.map(async (user: { username: string; id: string }) => {
@@ -42,38 +90,6 @@ export default async (body: any, faunaClient: faunadb.Client) => {
               }
             )
           )
-
-        const coreValues = [
-          ":be_kind:",
-          ":bring_the_magic:",
-          ":think_it_through:",
-          ":keep_it_simple:",
-          ":find_a_way:",
-          ":own_it:",
-          ":be_authentic:"
-        ]
-
-        const celebrateEmojis = [
-          ":highfive:",
-          ":dancing-mage:",
-          ":bananadance:",
-          ":party_blob:",
-          ":party_cat:",
-          ":partycorgi:",
-          ":dancing-dog:",
-          ":mario_luigi:"
-        ]
-
-        const successMessage = [
-          "Plusses for you",
-          "Way to go",
-          "Rock on",
-          "Keep it up"
-        ]
-
-        const getRandomValue = (array: string[]) => {
-          return array[Math.floor(Math.random() * array.length)]
-        }
 
         return `:sparkles:${getRandomValue(
           coreValues
